be17368cc3f153aabff8fb0dc17ef841df55157d
Nilam Patidar <patidarnilam11@gmail.com>
2023-04-05 15:47:13 +0530

    twillo sms

---
+++ config/initializers/twillo.rb
@@ -0,0 +1,5 @@
+require 'twilio-ruby'
+Twilio.configure do |config|
+    config.account_sid = Rails.application.secrets.twilio_account_sid
+    config.auth_token = Rails.application.secrets.twilio_auth_token
+end
